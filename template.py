dc79eba1c84ead38f966a8afbc12eb15a58e3b92
Enrique <erivera7240@gmail.com>
2024-03-18 03:04:10 -0500

    adding comments to template

--- template.py
+++ template.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import logging
 
+# logging.INFO lets us see the logs in the console( AKA the files being created)
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
@@ -26,12 +27,13 @@ list_of_files = [
     "templates/index.html"
 ]
 
-
+# go through the list of files and create them if they don't exist
 for filepath in list_of_files:
-    filepath = Path(filepath)
+
+    filepath = Path(filepath) # handles the path of the file for all OS
     filedir, filename = os.path.split(filepath)
 
-
+    # if the file is not created, we create it
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
@@ -41,6 +43,6 @@ for filepath in list_of_files:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
-
+    # if the file is already created, we log it
     else:
         logging.info(f"{filename} is already exists")
